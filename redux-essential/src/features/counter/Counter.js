7916aa018ab3eae9f28ad03906d3a4612096e6a9
nara <parknara0213@gmail.com>
2024-04-16 20:31:16 +0900

    240416 define  a react counter component

---
+++ redux-essential/src/features/counter/Counter.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  selectCount,
+} from "./counterSlice";
+import styles from "./Counter.module.css";
+
+const Counter = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+        <span className={styles.value}>{count}</span>
+        <button
+          className={styles.button}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
