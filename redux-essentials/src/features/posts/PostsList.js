334b71d9aab1bf6bba57934ffb526d259aa1dea6
nara <parknara0213@gmail.com>
2024-04-17 14:30:32 +0900

    240417 sort the postsList

--- redux-essentials/src/features/posts/PostsList.js
+++ redux-essentials/src/features/posts/PostsList.js
@@ -6,7 +6,11 @@ export const PostsList = () => {
   // read data from store state
   const posts = useSelector((state) => state.posts)
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
--- redux-essentials/src/features/posts/postsSlice.js
+++ redux-essentials/src/features/posts/postsSlice.js
@@ -1,8 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post', content: 'Hello!' },
-  { id: '2', title: 'Seconde Post', content: 'More text' },
+  {
+    id: '1',
+    title: 'First Post',
+    content: 'Hello!',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
+  {
+    id: '2',
+    title: 'Seconde Post',
+    content: 'More text',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
 ]
 
 const postsSlice = createSlice({
