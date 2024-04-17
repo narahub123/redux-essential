8f211332c0f5dc1a5097c678a0f428ab990be444
nara <parknara0213@gmail.com>
2024-04-17 14:03:06 +0900

    240417 prepare action payloads

--- redux-essentials/src/features/posts/postsSlice.js
+++ redux-essentials/src/features/posts/postsSlice.js
@@ -10,8 +10,19 @@ const postsSlice = createSlice({
   initialState,
   reducers: {
     // how to create reducer
-    postAdded(state, action) {
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        }
+      },
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
