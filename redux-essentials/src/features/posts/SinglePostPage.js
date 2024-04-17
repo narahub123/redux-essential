6fbf79364789138b88709e695feaecb694cf5a04
nara <parknara0213@gmail.com>
2024-04-17 13:45:14 +0900

    240417 create an edit post form

--- redux-essentials/src/features/posts/SinglePostPage.js
+++ redux-essentials/src/features/posts/SinglePostPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 // react router
 export const SinglePostPage = ({ match }) => {
@@ -21,6 +22,9 @@ export const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
+        <Link to={`/editPost/${post.id}`} className="button">
+          Edit Post
+        </Link>
       </article>
     </section>
   )
