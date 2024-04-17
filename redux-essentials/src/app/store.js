3d1d7199af1b89feabc6c79adcc198783b6d0635
nara <parknara0213@gmail.com>
2024-04-17 10:53:52 +0900

    240417 create posts slice

--- redux-essentials/src/app/store.js
+++ redux-essentials/src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import PostsReducer from '../features/posts/postsSlice'
+
 export const store = configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: PostsReducer,
+  },
 })
