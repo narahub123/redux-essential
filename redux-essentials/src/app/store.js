3255aa52d030016f0294cfa23a0b2cd53dcd58d7
nara <parknara0213@gmail.com>
2024-04-17 14:07:53 +0900

    240417 add a user slice

--- redux-essentials/src/app/store.js
+++ redux-essentials/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import PostsReducer from '../features/posts/postsSlice'
+import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
 
 export const store = configureStore({
   reducer: {
-    posts: PostsReducer,
+    posts: postsReducer,
+    users: usersReducer,
   },
 })
