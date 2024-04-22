586a3a86ef32ee849ecde188d7611fcf2bc353de
nara <parknara0213@gmail.com>
2024-04-22 13:26:48 +0900

    240422 refetch post manually

--- rtk-essentials/src/features/posts/PostsList.js
+++ rtk-essentials/src/features/posts/PostsList.js
@@ -1,21 +1,17 @@
-import React, { useEffect, useMemo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useMemo } from 'react'
+
 import { Link } from 'react-router-dom'
+
+import classnames from 'classnames'
+
 import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
-// import {
-//   fetchPosts,
-//   selectAllPosts,
-//   selectPostIds,
-//   selectPostById,
-// } from './postsSlice'
 
 import { useGetPostsQuery } from '../api/apiSlice'
 
 let PostExcerpt = ({ post }) => {
-  // const post = useSelector((state) => selectPostById(state, postId))
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
@@ -39,9 +35,11 @@ export const PostsList = () => {
   const {
     data: posts = [],
     isLoading,
+    isFetching,
     isSuccess,
     isError,
     error,
+    refetch,
   } = useGetPostsQuery()
 
   const sortedPosts = useMemo(() => {
@@ -55,15 +53,22 @@ export const PostsList = () => {
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = sortedPosts.map((post) => (
+    const renderedPosts = sortedPosts.map((post) => (
       <PostExcerpt key={post.id} post={post} />
     ))
+
+    const containerClassname = classnames('posts-container', {
+      disabled: isFetching,
+    })
+
+    content = <div className={containerClassname}>{renderedPosts}</div>
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
   return (
     <section className="posts-list">
       <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
       {content}
     </section>
   )
