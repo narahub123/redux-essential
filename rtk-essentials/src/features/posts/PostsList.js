bba67cfbb0269ca685fa572b7e379b98140eaac3
nara <parknara0213@gmail.com>
2024-04-22 12:49:07 +0900

    240422 use query hooks in components

--- rtk-essentials/src/features/posts/PostsList.js
+++ rtk-essentials/src/features/posts/PostsList.js
@@ -5,17 +5,19 @@ import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
-import {
-  fetchPosts,
-  selectAllPosts,
-  selectPostIds,
-  selectPostById,
-} from './postsSlice'
+// import {
+//   fetchPosts,
+//   selectAllPosts,
+//   selectPostIds,
+//   selectPostById,
+// } from './postsSlice'
 
-let PostExcerpt = ({ postId }) => {
-  const post = useSelector((state) => selectPostById(state, postId))
+import { useGetPostsQuery } from '../api/apiSlice'
+
+let PostExcerpt = ({ post }) => {
+  // const post = useSelector((state) => selectPostById(state, postId))
   return (
-    <article className="post-excerpt">
+    <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <div>
         <PostAuthor userId={post.user} />
@@ -34,30 +36,29 @@ let PostExcerpt = ({ postId }) => {
 PostExcerpt = React.memo(PostExcerpt)
 
 export const PostsList = () => {
-  const dispatch = useDispatch()
+  const { data: posts, isLoading, isSuccess, isError, error } = useGetPostsQuery
+  // const dispatch = useDispatch()
   // read data from store state
 
-  const orderedPostIds = useSelector(selectPostIds)
+  // const orderedPostIds = useSelector(selectPostIds)
   // const posts = useSelector(selectAllPosts)
   // const posts = useSelector((state) => state.posts)
 
-  const postStatus = useSelector((state) => state.posts.status)
-  const error = useSelector((state) => state.posts.error)
+  // const postStatus = useSelector((state) => state.posts.status)
+  // const error = useSelector((state) => state.posts.error)
 
-  useEffect(() => {
-    if (postStatus === 'idle') {
-      dispatch(fetchPosts())
-    }
-  }, [postStatus, dispatch])
+  // useEffect(() => {
+  //   if (postStatus === 'idle') {
+  //     dispatch(fetchPosts())
+  //   }
+  // }, [postStatus, dispatch])
 
   let content
 
-  if (postStatus === 'loading') {
+  if (isLoading) {
     content = <Spinner text="Loading..." />
-  } else if (postStatus === 'succeeded') {
-    content = orderedPostIds.map((postId) => (
-      <PostExcerpt key={postId} postId={postId} />
-    ))
+  } else if (isSuccess) {
+    content = posts.map((post) => <PostExcerpt key={post.id} post={post} />)
     // Sort posts in reverse chronological order by datetime string
     // const orderedPosts = posts
     //   .slice()
@@ -67,23 +68,9 @@ export const PostsList = () => {
     // content = orderedPosts.map((post) => (
     //   <PostExcerpt key={post.id} post={post} />
     // ))
-  } else if (postStatus === 'failed') {
-    content = <div>{error}</div>
+  } else if (isError) {
+    content = <div>{error.toString()}</div>
   }
-
-  // const orderedPosts = posts
-  //   .slice()
-  //   .sort((a, b) => b.date.localeCompare(a.date))
-
-  // const renderedPosts = orderedPosts.map((post) => (
-  //   <article className="post-excerpt" key={post.id}>
-  //     <h3>{post.title}</h3>
-  //     <p className="post-content">{post.content.substring(0, 100)}</p>
-  //     <Link to={`/posts/${post.id}`} className="button muted-button">
-  //       View Post
-  //     </Link>
-  //   </article>
-  // ))
   return (
     <section className="posts-list">
       <h2>Posts</h2>
