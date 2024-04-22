021552bdcea56db982154b7ad3db18f7d1ff488b
nara <parknara0213@gmail.com>
2024-04-22 12:31:02 +0900

    240422 define an api slice

---
+++ rtk-essentials/src/features/api/apiSlice.js
@@ -0,0 +1,21 @@
+// import the RTK query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// define a single API slice object
+export const apiSlice = createApi({
+  // the cache reducer expects to be added at 'state.api'(optional)
+  reducerPath: 'api',
+  // all of requests will have URLs starting with '/fakeApi/'
+  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  // the 'endpoit' represent operations and requests for the server
+  endpoints: (builder) => ({
+    // the 'getPosts' endpoint is a 'query' operation that return data
+    getPosts: builder.query({
+      // URL for the request is '/fakeApi/posts'
+      query: () => '/posts',
+    }),
+  }),
+})
+
+// export the auto-generated hook for the 'getPosts' query endpoint
+export const { useGetPostsQuery } = apiSlice
