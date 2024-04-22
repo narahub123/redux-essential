8422bcae2054ccf53d9c6e95061e1a91df07d685
nara <parknara0213@gmail.com>
2024-04-22 13:14:07 +0900

    240422 query arguments and cache keys

--- rtk-essentials/src/features/posts/SinglePostPage.js
+++ rtk-essentials/src/features/posts/SinglePostPage.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { selectPostById } from './postsSlice'
+
+import { Spinner } from '../../components/Spinner'
+import { useGetPostQuery, useGetPostsQuery } from '../api/apiSlice'
+
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 // react router
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector(selectPostById)
-  // const post = useSelector((state) =>
-  //   state.posts.find((post) => post.id === postId),
-  // )
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId)
+  // const post = useSelector(selectPostById)
+
+  let content
+
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
+      <article className="post">
+        <h2>{post.title}</h2>
+        <div>
+          <PostAuthor userId={post.userId} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+        <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
+        <Link to={`/editPost/${post.id}`} className="button">
+          Edit Post
+        </Link>
+      </article>
+    )
+  }
 
   if (!post) {
     return (
@@ -19,15 +43,5 @@ export const SinglePostPage = ({ match }) => {
       </section>
     )
   }
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-        <p className="post-content">{post.content}</p>
-        <Link to={`/editPost/${post.id}`} className="button">
-          Edit Post
-        </Link>
-      </article>
-    </section>
-  )
+  return <section>{content}</section>
 }
