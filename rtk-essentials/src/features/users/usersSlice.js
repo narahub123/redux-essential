ed4a91708462e2ffeecf8215bafd11fecff34506
nara <parknara0213@gmail.com>
2024-04-23 12:41:43 +0900

    240423 transform responses

--- rtk-essentials/src/features/users/usersSlice.js
+++ rtk-essentials/src/features/users/usersSlice.js
@@ -7,38 +7,28 @@ import {
 import { client } from '../../api/client'
 import { apiSlice } from '../api/apiSlice'
 
+const usersAdapter = createEntityAdapter()
+
+const initialState = usersAdapter.getInitialState()
+
 export const extendedApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => '/users',
+      transformResponse: (responseData) => {
+        return usersAdapter.setAll(initialState, responseData)
+      },
     }),
   }),
 })
 
 export const { useGetUsersQuery } = extendedApiSlice
 
-// const usersAdapter = createEntityAdapter()
-
-// const initialState = usersAdapter.getInitialState()
-
-// Calling `someEndpoint.select(someArg)` generates a new selector that will return
-// the query result object for a query with those parameters.
-// To generate a selector for a specific query argument, call `select(theQueryArg)`.
-// In this case, the users query has no params, so we don't pass anything to select()
 export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select()
-// export const selectUsersResult = apiSlice.endpoints.getUsers.select()
 
-const emptyUsers = []
-
-export const selectAllUsers = createSelector(
+const selectUsersData = createSelector(
   selectUsersResult,
-  (usersResult) => usersResult?.data ?? emptyUsers,
-)
-
-export const selectUserById = createSelector(
-  selectAllUsers,
-  (state, userId) => userId,
-  (users, userId) => users.find((user) => user.id === userId),
+  (usersResult) => usersResult.data,
 )
 
 const usersSlice = createSlice({
@@ -52,5 +42,5 @@ const usersSlice = createSlice({
 
 export default usersSlice.reducer
 
-// export const { selectAll: selectAllUsers, selectById: selectUserById } =
-//   usersAdapter.getSelectors((state) => state.users)
+export const { selectAll: selectAllUsers, selectById: selectUserById } =
+  usersAdapter.getSelectors((state) => selectUsersData(state) ?? initialState)
